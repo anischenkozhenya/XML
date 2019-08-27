89109575514ba171876ad2c5bb9ff0fcf7acf4f6
anischenkozhenya <43790223+anischenkozhenya@users.noreply.github.com>
2019-08-27 17:06:21 +0300

    +Task1

---
+++ XML/Task1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using System.Text;
+using System.Xml;
+//Создайте.xml файл, который соответствовал бы следующим требованиям: 
+//· имя файла: TelephoneBook.xml 
+//· корневой элемент: “MyContacts” 
+//· тег “Contact”, и в нем должно быть записано имя контакта и атрибут 
+//“TelephoneNumber” со значением номера телефона.
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Относительный путь для файла
+            string path = @"..\..\";
+            string fileName = @"TelephoneBook.xml";
+            //Открытие папки для наглядности
+            Process.Start("explorer.exe",path);
+            //Создание XML-файла с именем
+            var xmlnewfile = new XmlTextWriter(path+fileName, Encoding.UTF8);
+            xmlnewfile.WriteStartDocument();
+            //Создание корневого тега(только один)
+            xmlnewfile.WriteStartElement("MyContacts");
+            //Добавление контактов
+            xmlnewfile.AddContact("Eugene","+375445468555");
+            xmlnewfile.AddContact("Dima", "+375441234567");
+
+            xmlnewfile.WriteEndElement();
+            //Закрытие файла
+            xmlnewfile.Close();
+        }
+    }
+}
---
+++ XML/Task1/XMLCreater.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Task1
+{
+    public static class XMLCreater
+    {
+        /// <summary>
+        /// Добавление нового контакта(Метод расширения)
+        /// </summary>
+        /// <param name="xmlfile">объект XmlTextWriter</param>
+        /// <param name="name">Имя</param>
+        /// <param name="phoneNumber">Номер телефона</param>
+        public static void AddContact(this XmlTextWriter xmlfile, string name,string phoneNumber)
+        {            
+            xmlfile.WriteStartElement("Contact");
+            xmlfile.WriteStartAttribute("TelephoneNumber");
+            xmlfile.WriteString(phoneNumber);
+            xmlfile.WriteEndAttribute();
+            xmlfile.WriteString(name);
+            xmlfile.WriteEndElement();            
+        }
+    }
+}
